b27fb192cb19a2ffb79da6fabffe75f9f3fd39ea
yoneey <yago.157.e.y@gmail.com>
2026-09-21 19:09:09 -0300

    mudanças visuais minimas

--- front/src/Historico.tsx
+++ front/src/Historico.tsx
@@ -2,6 +2,7 @@ import './Historico.css';
 
 function Historico() {
     const pautas = ['Pauta 1', 'Pauta 2', 'Pauta 3'];
+    const filtros = ['Filtro 1', 'Filtro 2', 'Filtro 3', 'Filtro 4', 'Filtro 5', 'Filtro 6', 'Filtro 7', 'Filtro 8', 'Filtro 9', 'Filtro 10'];
 
   return (
     <section className="pagina">
@@ -18,7 +19,15 @@ function Historico() {
                 <h2>Filtros</h2>
 
                 <div className="filtros">
-
+                    <ul className="ListaFiltros">
+                        {filtros.map((filtro) => (
+                            <li key={filtro}>
+                                <button type="button" className="FiltroButton">
+                                    {filtro}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
 
                 <div>
